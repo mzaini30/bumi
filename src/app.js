a8178d9859dba0b3b5436bf14b1cb90e9cebac3a
Zen <muhzaini30@gmail.com>
2022-10-29 23:52:27 +0800

    perbaiki bug

--- src/app.js
+++ src/app.js
@@ -1,39 +1,45 @@
 #!/usr/bin/env node
 
-import commonjs from '@rollup/plugin-commonjs'
-import {nodeResolve} from '@rollup/plugin-node-resolve'
-import {uglify} from 'rollup-plugin-uglify'
-import {rollup} from 'rollup'
-import {readFileSync, writeFileSync} from 'fs'
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { uglify } from "rollup-plugin-uglify";
+import { rollup } from "rollup";
+import { readFileSync, writeFileSync } from "fs";
 
-const namaFile = process.argv[2]
+const namaFile = process.argv[2];
 // const namaVariabel = process.argv[3]
 
-const outputFile = namaFile.replace(/(\.js)$/, '.min.js')
-											 .replace(/(\.mjs)$/, '.min.js')
-											 .replace(/(\.cjs)$/, '.min.js')
+const outputFile = namaFile
+  .replace(/(\.js)$/, ".min.js")
+  .replace(/(\.mjs)$/, ".min.js")
+  .replace(/(\.cjs)$/, ".min.js");
+
+const nama_variabel = namaFile
+  .replace(/(\.js)$/, "")
+  .replace(/(\.mjs)$/, "")
+  .replace(/(\.cjs)$/, "");
 
 const inputOptions = {
-	input: namaFile,
-	plugins: [nodeResolve(), commonjs()]
-}
+  input: namaFile,
+  plugins: [nodeResolve(), commonjs()],
+};
 const outputOptions = {
-	file: outputFile,
-	format: 'umd',
-	name: namaFile,
-	plugins: [uglify()]
-}
+  file: outputFile,
+  format: "umd",
+  name: nama_variabel,
+  plugins: [uglify()],
+};
 
-async function build(){
-	const bundle = await rollup(inputOptions)
-	const {output} = await bundle.generate(outputOptions)
-	const selesai = await bundle.write(outputOptions)
-	if (selesai) {
-		const hasilnya = readFileSync(outputFile).toString()
-		const tambahan = `// Nama variabel: ${namaFile}`
-		const jadinya = `${tambahan}\n${hasilnya}`
-		writeFileSync(outputFile, jadinya)
-	}
-	await bundle.close()
+async function build() {
+  const bundle = await rollup(inputOptions);
+  const { output } = await bundle.generate(outputOptions);
+  const selesai = await bundle.write(outputOptions);
+  if (selesai) {
+    const hasilnya = readFileSync(outputFile).toString();
+    const tambahan = `// Nama variabel: ${nama_variabel}`;
+    const jadinya = `${tambahan}\n${hasilnya}`;
+    writeFileSync(outputFile, jadinya);
+  }
+  await bundle.close();
 }
-build()
+build();
